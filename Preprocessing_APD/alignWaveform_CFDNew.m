3220bcef3075a97cc81fa9d1dd23b9fd47a4d775
Xiangnan Zhou <xnzhou@ucdavis.edu>
2021-02-24 20:28:56 -0800

    new CFD zero crossing detection method
    new CFD alignment file, compute zero crossing by detect data point sign change

---
+++ Preprocessing_APD/alignWaveform_CFDNew.m
@@ -0,0 +1,64 @@
+function [data_out] = alignWaveform_CFDNew(data_in,t_rising, dt,f)
+% in-input matrix with waveform as columns
+% f is fraction
+% t_rising is the rising time of the signal
+% dt is sampling interval
+% data with peak aligned 
+switch nargin
+    case 3
+        f = 0.5;
+    case 4
+        
+    otherwise
+        error('Wrong number of Inputs')
+end
+% remove NaN
+data_in_raw = data_in; % make copy of raw data
+data_in(isnan(data_in)) = eps;
+
+% remove signal DC value
+DC = mean(data_in(1:75));
+data_in = data_in-DC;
+% figure;plot(data_in(:,1:50:end));
+
+% delay positive part
+t_d = ceil(t_rising/dt*0.5);
+dataDelayed = circshift(data_in,t_d,1);
+%
+dataInvert = -data_in*f;
+
+% 
+dataSum = dataDelayed+dataInvert;
+% figure;plot(dataSum(:,4)); hold on; plot(dataInvert(:,4));plot(dataDelayed(:,4));hold off
+[~,minIdx] = min(dataInvert);
+minIdx = round(mean(minIdx))-1;
+if minIdx<1
+    minIdx = 1;
+end
+    
+[~,maxIdx] = max(dataDelayed);
+maxIdx = round(mean(maxIdx))+1;
+
+% plotIdx = 100;
+% figure;plot(dataDelayed(:,plotIdx));hold on;plot(dataSum(:,plotIdx));hold off; 
+% pause(1);
+% close(gcf);
+risingEdge = dataSum(minIdx:maxIdx,:);
+zeroCrossingIdx =zeros(size(risingEdge,2),1);
+
+for i = 1:size(risingEdge,2)
+    risingEdgeShifted = circshift(risingEdge(:,i),-1);
+    idxTemp = find((risingEdgeShifted.*risingEdge(:,i))<=0,1);
+    if isempty(idxTemp)
+        idxTemp = 0;
+    end
+    zeroCrossingIdx(i) = idxTemp+1;
+end
+[~,zeroCrossingIdxMin] = min(abs(risingEdge));
+zeroCrossingIdxMin = zeroCrossingIdxMin';
+shift = mode(zeroCrossingIdx)-zeroCrossingIdx; % calculate shift
+data_out = data_in_raw;
+for i = 1:length(shift) %loop through all columns
+    data_out(:,i) = circshift(data_in_raw(:,i),shift(i));
+end
+%data_out = downsample(data_out,5);
