6feb1a0e300602c7ab70535315a39db0b0b41f62
Xiangnan Zhou <xnzhou@ucdavis.edu>
2021-04-12 11:58:31 -0700

    update code to load old data file
    updated FLImData class to be able to load old data file
    added pop window asking parameters if it is not available from data file

--- Preprocessing_APD/FLImDataClass.m
+++ Preprocessing_APD/FLImDataClass.m
@@ -22,26 +22,58 @@ classdef FLImDataClass < handle
         function loadFromfile(FLImDataObj)
             [output,~] = TDMS_getStruct(FLImDataObj.filePath);
             FLImDataObj.numOfPoints = output.Channel1.Props.Num_of_points;
+            try
             FLImDataObj.dataAvg = output.Props.WFAverage;
+            catch
+                FLImDataObj.dataAvg = output.Channel1.Props.Num_of_points/length(output.Channel1.Voltage.data);
+            end
+            
+            try
             FLImDataObj.laserRepRate = double(output.Props.LaserRepRate);
+            catch
+                prompt = 'No laser rep rate information available from data file. Please manually input laser rep rate!';
+                dlgtitle = 'Please input laser rep rate';
+                dims = [1 70];
+                definput = {'250'};
+                answer = inputdlg(prompt,dlgtitle,dims,definput);
+                FLImDataObj.laserRepRate = str2double(answer{1});
+            end
 %             FLImDataObj.laserRepRate; % output laser reprate to 
             
             timeDelay = [4.2800;4.9563;5.0427]; % time delay in ms
             shift = round(timeDelay./1000.*FLImDataObj.laserRepRate);
             FLImDataObj.WFLength = output.Channel1.Props.Data_Length;
             FLImDataObj.ch1RawWF = reshape(output.Channel1.Waveform.data,FLImDataObj.WFLength,FLImDataObj.numOfPoints);
-            FLImDataObj.V1 = output.Channel1.Voltage.data';
+            % check data and V dimention, if the same, load data, if not duplicate V
             
+            if size(FLImDataObj.ch1RawWF,2)==length(output.Channel1.Voltage.data)
+                FLImDataObj.V1 = output.Channel1.Voltage.data';
+            else
+                temp = repmat(output.Channel1.Voltage.data,FLImDataObj.dataAvg,1);
+                FLImDataObj.V1 = temp(:);
+            end
             FLImDataObj.ch1RawWF = circshift(FLImDataObj.ch1RawWF,-shift(1),2); % circular shift to account for delay
             %             FLImDataObj.V1 = circshift(FLImDataObj.V1,-1); % circular shift to account for delay
             FLImDataObj.ch2RawWF = reshape(output.Channel2.Waveform.data,FLImDataObj.WFLength,FLImDataObj.numOfPoints);
-            FLImDataObj.V2 = output.Channel2.Voltage.data';
+            if size(FLImDataObj.ch2RawWF,2)==length(output.Channel2.Voltage.data)
+                FLImDataObj.V2 = output.Channel2.Voltage.data';
+            else
+                temp = repmat(output.Channel2.Voltage.data,FLImDataObj.dataAvg,1);
+                FLImDataObj.V2 = temp(:);
+            end
             FLImDataObj.ch2RawWF = circshift(FLImDataObj.ch2RawWF,-shift(2),2); % circular shift to account for delay
             %             FLImDataObj.V2 = circshift(FLImDataObj.V2,-1); % circular shift to account for delay
             FLImDataObj.ch3RawWF = reshape(output.Channel3.Waveform.data,FLImDataObj.WFLength,FLImDataObj.numOfPoints);
             FLImDataObj.V3 = output.Channel3.Voltage.data';
+            if size(FLImDataObj.ch3RawWF,2)==length(output.Channel3.Voltage.data)
+                FLImDataObj.V3 = output.Channel3.Voltage.data';
+            else
+                temp = repmat(output.Channel3.Voltage.data,FLImDataObj.dataAvg,1);
+                FLImDataObj.V3 = temp(:);
+            end
             FLImDataObj.ch3RawWF = circshift(FLImDataObj.ch3RawWF,-shift(3),2); % circular shift to account for delay
             %             FLImDataObj.V3 = circshift(FLImDataObj.V3,-1); % circular shift to account for delay
+            
             checkDataIntegraty(FLImDataObj)
         end
         
