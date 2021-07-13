13b15a203a6be32d05425fa050e621f86e01d14b
Xiangnan Zhou <xnzhou@ucdavis.edu>
2021-07-12 19:48:41 -0700

    added voltage and gain info to background object
    show voltage and background info on bg plot

--- Preprocessing/backGround.m
+++ Preprocessing/backGround.m
@@ -4,6 +4,12 @@ classdef backGround < handle
         bgCh1; %Array n by 1
         bgCh2; %Array n by 1
         bgCh3; %Array n by 1
+        CtrlV1; % scaler
+        CtrlV2; % scaler
+        CtrlV3; % scaler
+        bgGain1 = NaN; % scaler gain value
+        bgGain2 = NaN; % scaler gain value
+        bgGain3 = NaN; % scaler gain value
         pathToBg % full path to bg
     end
     
@@ -20,7 +26,10 @@ classdef backGround < handle
             bg3 = output.Channel_3.AvgBgData.data;
             obj.bgCh1 = bg1';
             obj.bgCh2 = bg2';
-            obj.bgCh3 = bg3';             
+            obj.bgCh3 = bg3';
+            obj.CtrlV1 = output.Channel_1.CtrlV.data(1);
+            obj.CtrlV2 = output.Channel_2.CtrlV.data(1);
+            obj.CtrlV3 = output.Channel_3.CtrlV.data(1);
             catch
                bg1 = output.Background.Ch1.data;
                bg2 = output.Background.Ch2.data;
@@ -28,6 +37,9 @@ classdef backGround < handle
                obj.bgCh1 = bg1';
                obj.bgCh2 = bg2';
                obj.bgCh3 = bg3';    
+               obj.CtrlV1 = NaN;
+               obj.CtrlV2 = NaN;
+               obj.CtrlV3 = NaN;
             end
         end
     end
