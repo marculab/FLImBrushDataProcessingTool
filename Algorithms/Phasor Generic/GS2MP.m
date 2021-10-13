5f1cf36ff6d974f857b7dfc70374f611bc70d9af
Xiangnan Zhou <xnzhou@ucdavis.edu>
2021-10-12 21:20:55 -0700

    removed unknown phasor code

--- Algorithms/Phasor Generic/GS2MP.m
+++
@@ -1,4 +0,0 @@
-function [M,P] = GS2MP(G,S,wtau)
-M = sqrt(G.^2 + S.^2)./sqrt(1./(1+wtau.^2));
-P = atan2(S,G) + atan(wtau);
-end
--- Algorithms/Phasor Generic/MPcorrection.m
+++
@@ -1,4 +0,0 @@
-function [Gout,Sout] = MPcorrection(G,S,M,P)
-Gout = (G.*cos(P) + S.*sin(P))./M;
-Sout = (-G.*sin(P) + S.*cos(P))./M;
-end
