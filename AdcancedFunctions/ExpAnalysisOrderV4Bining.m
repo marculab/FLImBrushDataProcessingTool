df70b357e4a253cc87c12121a112c631e13b2341
Xiangnan Zhou <xnzhou@ucdavis.edu>
2021-12-14 13:48:05 -0800

    updated code for V4 analysis

--- AdcancedFunctions/ExpAnalysisOrderV4Bining.m
+++ AdcancedFunctions/ExpAnalysisOrderV4Bining.m
@@ -21,7 +21,7 @@ else
 end
 for k = 1:numOfFiles
     % load in file
-    if k==1
+    if numOfFiles==1
         P = fullfile(DeConpath,DeConfile);
         [filepath,name,ext]=fileparts(DeConfile);
     else
@@ -29,7 +29,7 @@ for k = 1:numOfFiles
         [filepath,name,ext]=fileparts(DeConfile{k});
     end
     load(P)
-    
+    name
     %% SNR filter data
     dataObj = Data{2}; %% set up your channel here
     WFAligned = dataObj.channeldata.data;
@@ -41,10 +41,10 @@ for k = 1:numOfFiles
     n_max = floor(log2(numOfWF));
     WFAligned = WFAligned(:,end-2^n_max+1:end); % pick 2^n_max waveforms
     %%
-    figure('units','pixels','outerposition',[100 100 1920 1080])
-    tiledlayout(3,3)
+    %     figure('units','pixels','outerposition',[100 100 1920 1080])
+    %     tiledlayout(3,3)
     
-    for c = 0:n_max
+    for c = n_max
         % get Laguerre result
         wfAvg = power(2,c);
         numOfAveWf = size(WFAligned,2)/wfAvg;
@@ -72,93 +72,19 @@ for k = 1:numOfFiles
         LT3Decay = h_lifet(decay3,0.08);
         W3 = A3./sum(A3);
         FC3 = A3.*T3./sum(A3.*T3);
-        %% tri-exponential fit
-        % order4 = 4;
-        % [A4, T4, avglife4, intensity4, fit4, raw4, decay4] = multiexp_fit(WF,0.08,irf,order4,[]);
-        % res4 = WF-fit4;
-        % res4SE = sum(res4.^2);
-        % LT4Decay = h_lifet(decay4,0.08);
-        % W4 = A4./sum(A4);
         
-        %% plot result
-        nexttile
-        scatter(FC3(1,:),T3(1,:),'bo','LineWidth',2)
-        hold on
-        scatter(FC3(2,:),T3(2,:),'g+','LineWidth',2)
-        scatter(FC3(3,:),T3(3,:),SNR_new,'rs','LineWidth',2)
-        hold off
-        set(gca, 'YScale', 'log')
-        legend('exp1','exp2','exp3')
-        xlabel('Fractional Contribution')
-        ylabel('Lifetime (ns)')
-        xlim([0 1])
-        ylim([0.01 50])
-        grid on
-        grid minor
-        title(sprintf('Averaging = %d, # of wavefroms = %d',2^c,numOfAveWf));
-        
-        
-        % idx = 1;
-        %
-        % % plot bi-exponential
-        % nexttile
-        % plot(WF(:,idx),'b.','MarkerSize',10)
-        % hold on
-        % plot(fit2(:,idx),'r-','LineWidth',1)
-        % plot(res2(:,idx)-0.1,'m.')
-        % yline(-0.1+0.025,'r--','res=0.025')
-        % yline(-0.1-0.025,'r--')
-        % text(150, -0.15, ['\Sigma res = ', num2str(res2SE)])
-        % yticks((-0.2:0.1:1))
-        % yticklabels({'','res=0','0','0.1','0.2','0.3','0.4','0.5','0.6','0.7','0.8','0.9','1.0',})
-        % grid on
-        % grid minor
-        % axis tight
-        % ylim([-0.2 1])
-        % legend('raw data', 'fitting', 'residue')
-        % title(sprintf('Channel %d, Exponential fitting, order = %d, WF index %d,\n lifetime = %.3f(formula), lifetime = %.3f(decay),\n a1=%.3f, a2=%.3f, tau1=%.3f, tau2=%.3f', ...
-        %     c, order2,idx, avglife2(idx), LT2Decay(idx), W2(1), W2(2), T2(1), T2(2)))
-        %
-        % % plot tri-exponential
-        % nexttile
-        % plot(WF(:,idx),'b.','MarkerSize',10)
-        % hold on
-        % plot(fit3(:,idx),'r-','LineWidth',1)
-        % plot(res3(:,idx)-0.1,'m.')
-        % yline(-0.1+0.025,'r--','res=0.025')
-        % yline(-0.1-0.025,'r--')
-        % text(150, -0.15, ['\Sigma res = ', num2str(res3SE)])
-        % yticks((-0.2:0.1:1))
-        % yticklabels({'','res=0','0','0.1','0.2','0.3','0.4','0.5','0.6','0.7','0.8','0.9','1.0',})
-        % grid on
-        % grid minor
-        % axis tight
-        % ylim([-0.2 1])
-        % legend('raw data', 'fitting', 'residue')
-        % title(sprintf('Channel %d, Exponential fitting, order = %d, WF index %d,\n lifetime = %.3f(formula), lifetime = %.3f(decay),\n a1=%.3f, a2=%.3f, a3=%.3f, tau1=%.3f, tau2=%.3f, tau3=%.3f', ...
-        %     c, order3,idx, avglife3(idx), LT3Decay(idx), W3(1), W3(2), W3(3), T3(1), T3(2), T3(3)))
-        %
-        % % plot quad-exponential
-        % nexttile
-        % plot(WF(:,idx),'b.','MarkerSize',10)
-        % hold on
-        % plot(fit4(:,idx),'r-','LineWidth',1)
-        % plot(res4(:,idx)-0.1,'m.')
-        % yline(-0.1+0.025,'r--','res=0.025')
-        % yline(-0.1-0.025,'r--')
-        % text(150, -0.15, ['\Sigma res = ', num2str(res4SE)])
-        % yticks((-0.2:0.1:1))
-        % yticklabels({'','res=0','0','0.1','0.2','0.3','0.4','0.5','0.6','0.7','0.8','0.9','1.0',})
-        % grid on
-        % grid minor
-        % axis tight
-        % ylim([-0.2 1])
-        % legend('raw data', 'fitting', 'residue')
-        % title(sprintf('Channel %d, Exponential fitting, order = %d, WF index %d,\n lifetime = %.3f(formula), lifetime = %.3f(decay),\n a1=%.3f, a2=%.3f, a3=%.3f, a4=%.3f, tau1=%.3f, tau2=%.3f, tau3=%.3f, tau4=%.3f', ...
-        %     c, order4, idx, avglife4(idx), LT4Decay(idx), W4(1), W4(2), W4(3), W4(4), T4(1), T4(2), T4(3), T4(4)))
         
     end
+    dataForPhasor.rawWF = WFAligned;
+    dataForPhasor.iRF = irf;
+    dataForPhasor.a = A3;
+    dataForPhasor.tau = T3;
+    dataForPhasor.frac_Contribution = FC3;
     
-    exportgraphics(gcf,fullfile(DeConpath,[name,'Ch2.tiff']),'Resolution',600)
+    save(fullfile(DeConpath,[name,'_Ch2_Phasor.mat']),'dataForPhasor');
+    
+    
+    %     exportgraphics(gcf,fullfile(DeConpath,[name,'Ch2.tiff']),'Resolution',600)
 end
+
 close all
