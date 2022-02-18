ccc87ee431c019fb49ea4d898ea0ed787fe27965
Xiangnan Zhou <xnzhou@ucdavis.edu>
2022-02-18 09:56:42 -0800

    updated simulation parameters

--- AlphaDeterminatin/SimulationSingleExpAPD.m
+++ AlphaDeterminatin/SimulationSingleExpAPD.m
@@ -20,8 +20,8 @@ xlabel('Lifetime (ns)')
 ylabel('Count')
 
 %% get decays
-dt = 0.1;
-tWindow = 154; % ns same for V4 nad V5
+dt = 0.08;
+tWindow = 60; % ns same for V4 nad V5
 
 t = 0:dt:tWindow-dt;
 decay = zeros(round(tWindow/dt),N);
@@ -41,6 +41,7 @@ title(['Decay wiht lifetime ' num2str(trueLT(17))])
 %% load and truncate irf
 irfStruc = load('..\APDDetectorFile\M00549707_DCS.mat');
 UpFactor = irfStruc.irfRawdt/dt;
+UpFactor = round(UpFactor);
 irf = interp(irfStruc.irf(:,200),UpFactor);
 % irf = circshift(irf,266-155);
 [~,irfMaxIdx] = max(irf);
@@ -55,7 +56,7 @@ catch
     irfT = irf(startIdx:end);
 end
 [~,MaxIdx] = max(irfT);
-irfT = circshift(irfT,266-MaxIdx);
+% irfT = circshift(irfT,266-MaxIdx);
 figure
 plot(irfT)
 xlim([0 500])
@@ -65,11 +66,11 @@ title('irf')
 %%
 spec = filter(irfT,1,decay);
 spec = spec./max(spec);
-ref = spec*0.1;
-ref = circshift(ref, 32/dt);
-ref(641:end,:) = zeros(size(ref(641:end,:)));
-ref(1:539,:) = zeros(size(ref(1:539,:)));
-SNR = 49; %in dB, SNR = 20log10(Max/noise)
+ref = spec*0;
+% ref = circshift(ref, 32/dt);
+% ref(641:end,:) = zeros(size(ref(641:end,:)));
+% ref(1:539,:) = zeros(size(ref(1:539,:)));
+SNR = 50; %in dB, SNR = 20log10(Max/noise)
 if SNR==0
     noise = zeros(size(spec));
 else
@@ -99,14 +100,14 @@ alphaUpperLim=alpha_up(size(spec,1),LagOrder,[],[]);
 
 numOfAlpha = 1;
 % alphaVector = linspace(0.6,alphaUpperLim,numOfAlpha);
-alphaVector = 0.965; % 0.88 for 0.6-6, 0.95
+alphaVector = 0.916; % 0.88 for 0.6-6, 0.95
 LTArray = zeros(N,numOfAlpha);
 f = waitbar(0,'Starting');
 for i=1:numOfAlpha
     alphaTemp = alphaVector(i);
     channelDataStruct = ChannelData(spec,irfT,dt,1.5,1:size(spec,2),[],1800);
     Laguerre_Struct = LaguerreModel(channelDataStruct,LagOrder,alphaTemp);
-    Laguerre_Struct.estimate_laguerre([540:640],0);
+    Laguerre_Struct.estimate_laguerre([],0);
     LTArray(:,i) = Laguerre_Struct.LTs;
     waitbar(i/numOfAlpha,f,sprintf('Progress: %d %%',round(i/numOfAlpha*100)));
 end
