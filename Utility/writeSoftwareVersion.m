8f4ed5b9dbaeb12d10678b472775f3ea5d6319c1
Xiangnan Zhou <xnzhou@ucdavis.edu>
2022-06-16 15:21:07 -0700

    V1.0.0 moved file to Utility folder
    moved icon.png and writeSoftawreVersion.m file into Utility folder

--- Utility/writeSoftwareVersion.m
+++ Utility/writeSoftwareVersion.m
@@ -3,4 +3,5 @@ ini.AddSections({'Software Version'});
 ini.AddKeys('Software Version', 'MAJOR', 1);
 ini.AddKeys('Software Version', 'MINOR', 0);
 ini.AddKeys('Software Version', 'PATCH', 0);
-ini.WriteFile(fullfile(pwd,'SoftwareVersion.ini'));
+[filepath,name,ext] = fileparts(pwd);
+ini.WriteFile(fullfile(filepath,'SoftwareVersion.ini'));
