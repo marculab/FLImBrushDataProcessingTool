57e8f64422f79ffcf6dc888cfd140a2e2395341f
Xiangnan Zhou <xnzhou@ucdavis.edu>
2023-02-08 21:11:54 -0800

    added code to set iRF after certain CtrlV to the same

--- Algorithms/Preprocessing_APD/ADPTdmsToMat.m
+++ Algorithms/Preprocessing_APD/ADPTdmsToMat.m
@@ -1,6 +1,6 @@
 % covert APD detector tdms file to .mat file
 %%
-clear 
+clear
 close all
 clc
 %% select tdms file
@@ -37,19 +37,19 @@ for n = 1: numOfFiles
     irfRawdt = output.iRF.Props.dt;
     iRFName = fieldnames(output.iRFRaw);
     numOfIrf = length(iRFName)-2;
-    try 
-    lengthOfIRF = output.iRFRaw.Props.WFLength;
+    try
+        lengthOfIRF = output.iRFRaw.Props.WFLength;
     catch
         lengthOfIRF = 800;
     end
     
     try
-    numOfiRFPerV = output.iRFRaw.Props.NumOfWFsPerV;
+        numOfiRFPerV = output.iRFRaw.Props.NumOfWFsPerV;
     catch
         numOfiRFPerV = output.iRFRaw.Props.DataLength/lengthOfIRF;
     end
     iRF = zeros(lengthOfIRF,numOfIrf);
-    iRFUpSampled = zeros(lengthOfIRF*upSampleFactor,numOfIrf);
+    
     iRFV = zeros(numOfIrf,1);
     
     find_c = strfind(iRFName, 'c');
@@ -62,44 +62,59 @@ for n = 1: numOfFiles
         iRFRawtempUpSampled = zeros(lengthOfIRF*upSampleFactor,numOfiRFPerV);
         iRFV(i) = sscanf(iRFNameNumOnly{i},'c%d')*0.01;
         iRFTemp = getfield(output.iRFRaw,iRFName{i});
-%         iRFRawtemp = reshape(iRFTemp.data,lengthOfIRF,numOfiRFPerV);
+        %         iRFRawtemp = reshape(iRFTemp.data,lengthOfIRF,numOfiRFPerV);
         iRFRawtemp = reshape(iRFTemp.data,lengthOfIRF,[]);
         iRFRawtemp(:,sum(iRFRawtemp)==0) = [];
-        for m = 1:min(numOfiRFPerV,size(iRFRawtemp,2)) % upsample rawa data
-            iRFRawtempUpSampled(:,m) = interp(iRFRawtemp(:,m),upSampleFactor);
-        end
+        %         iRFRawtempAvg = mean(iRFRawtemp,2);
+        %         for m = 1:min(numOfiRFPerV,size(iRFRawtemp,2)) % upsample rawa data
+        %             iRFRawtempUpSampled(:,m) = interp(iRFRawtemp(:,m),upSampleFactor);
+        %         end
         %                 figure; plot(iRFRawtemp);title('Before alignment');xlim([200 230])
-        iRFRawtemp = alignWaveform_CFDNew(iRFRawtemp, 3, irfRawdt,0.5);
+        %         iRFRawtemp = alignWaveform_CFDNew(iRFRawtemp, 3, irfRawdt,0.5);
         iRF(:,i) = mean(iRFRawtemp,2);
-        iRFRawtempUpSampled(:,sum(iRFRawtempUpSampled)==0) = [];
-        iRFRawtempUpSampledAligned = alignWaveform_CFDNew(iRFRawtempUpSampled, 3, irfdt, 0.5); % CFD alignment
+        %         iRFUpSampled(:,i) = interp(iRF(:,i),upSampleFactor);
+        %         figure
+        %         plot(iRFRawtempAvg);
+        %         hold on
+        %         plot(iRF(:,i));
+        %         hold off
+        %         xlim([150 500])
+        %         iRFRawtempUpSampled(:,sum(iRFRawtempUpSampled)==0) = [];
+        %         iRFRawtempUpSampledAligned = alignWaveform_CFDNew(iRFRawtempUpSampled, 3, irfdt, 0.5); % CFD alignment
         %         xlim([400 460])
-%                 if i==225
-%                    figure;tiledlayout(1,2);nexttile;plot(iRFRawtempUpSampled);
-%                    nexttile;plot(iRFRawtempUpSampledAligned);
-%                 end
+        %                 if i==225
+        %                    figure;tiledlayout(1,2);nexttile;plot(iRFRawtempUpSampled);
+        %                    nexttile;plot(iRFRawtempUpSampledAligned);
+        %                 end
         %                 outlierIdx = isoutlier(max(iRFRawtemp));
-        iRFUpSampled(:,i) = mean(iRFRawtempUpSampledAligned,2);
+        %         iRFUpSampled(:,i) = mean(iRFRawtempUpSampledAligned,2);
     end
     
     [iRFV,I] = sort(iRFV,'ascend');
     iRF = iRF(:,I);
-    iRFUpSampled = iRFUpSampled(:,I);
+%     Idx = 150; % after this index, irf do not change anymore
+%     iRF(:,Idx) = mean(iRF(:,Idx+10),2);
+%     iRF(:,Idx+1:end) = [];
+%     iRFV(Idx+1:end) = [];
     %             iRF2G = interp1(Ch2CtrlV, Ch2Gain,Ch2iRFV,'makima');
     DC = mean(iRF(1:100,:));
     iRF = iRF-DC;
+    iRFUpSampled = zeros(lengthOfIRF*upSampleFactor,size(iRF,2));
+    for i = 1:size(iRF,2)
+        iRFUpSampled(:,i) = interp(iRF(:,i),upSampleFactor);
+    end
     irfV = iRFV;
     irf = iRF;
-    DCUp = mean(iRFUpSampled(1:200,:)); % compute DC
-    iRFUpSampled = iRFUpSampled-DCUp; % remove DC
+%     DCUp = mean(iRFUpSampled(end-100*upSampleFactor:end,:)); % compute DC
+%     iRFUpSampled = iRFUpSampled-DCUp; % remove DC
     irfUpSampled = iRFUpSampled;
-    saveName = [saveName '.mat'];
-%     save(fullfile(path,saveName),'serialNumber','dateModified','user','gainV','gain','irfV','irf','irfUpSampled','irfRawdt','irfdt');
+    saveName = [saveName ' DC front no CFD.mat'];
+    save(fullfile(path,saveName),'serialNumber','dateModified','user','gainV','gain','irfV','irf','irfUpSampled','irfRawdt','irfdt');
 end
 f = msgbox('Data Processing Completed');
 %% plot result
 figure
-idx = 170;
+idx = 150;
 x = 0:size(irf,1)-1;
 plot(x,irf(:,idx));
 hold on
@@ -107,4 +122,13 @@ xx = 0:1/upSampleFactor:size(irf,1)-1/upSampleFactor;
 plot(xx,irfUpSampled(:,idx));
 hold off
 % xlim([100 600])
-
+%%
+irfN = irf./max(irf);
+StartIdx = 210;
+figure
+plot(irfN(:,[StartIdx:StartIdx+10]))
+grid on
+xlim([180 500])
+% title('Ch3 irf idx 150-160')
+irfV(StartIdx)
+interp1(gainV,gain,irfV(StartIdx))
