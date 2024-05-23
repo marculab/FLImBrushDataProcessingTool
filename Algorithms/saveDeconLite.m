63d0fadb92abb774f81ed6847daa3fbc6dde8e10
Xiangnan Zhou <xnzhou@ucdavis.edu>
2024-05-22 21:30:13 -0700

    added Chi2 to decon lite
    added code to handle both 3 and 4 channel data

--- Algorithms/saveDeconLite.m
+++ Algorithms/saveDeconLite.m
@@ -1,9 +1,18 @@
-function saveDeconLite(Ch1DataObjIn,Ch2DataObjIn,Ch3DataObjIn,saveFileFullPath)
+function saveDeconLite(Ch1DataObjIn,Ch2DataObjIn,Ch3DataObjIn,Ch4DataObjIn,saveFileFullPath)
+if isempty(Ch4DataObjIn)
+    numOfChannel = 3;
+else
+    numOfChannel = 4;
+end
 Ch1DataObj = struct;
 Ch2DataObj = struct;
 Ch3DataObj = struct;
-VarList={'laserRepRate','alpha', 'dataT', 'gain','irfIdx','Lg_INTs','Lg_INTsGainCorrected', 'K', 'LaguerreBasis', 'Lg_LCs','Lg_LTs','noise','SNR','Ph_H1S','Ph_H1G','Ph_H2S','Ph_H2G','Ph_H3S','Ph_H3G','Ph_H4S','Ph_H4G'};
-for ii=1:3
+Ch4DataObj = struct;
+VarList={'laserRepRate','alpha', 'dataT', 'gain','irfIdx','Lg_INTs',...
+    'Lg_INTsGainCorrected', 'K', 'LaguerreBasis', 'Lg_LCs','Lg_LTs',...
+    'noise','SNR','stat_test.chi2','Ph_H1S','Ph_H1G','Ph_H2S','Ph_H2G',...
+    'Ph_H3S','Ph_H3G','Ph_H4S','Ph_H4G'};
+for ii=1:numOfChannel
     for jj=1:length(VarList)
         eval(['Ch' num2str(ii) 'DataObj.' VarList{jj} '=Ch' num2str(ii) 'DataObjIn.' VarList{jj} ';'])
         % eval(['SP_G' '=data.SP_G'';'])
@@ -13,4 +22,9 @@ end
 [filepath,name,ext] = fileparts(saveFileFullPath);
 name = strcat(name,'_lite',ext);
 new_filePath = fullfile(filepath,name);
-save(new_filePath,'Ch1DataObj','Ch2DataObj','Ch3DataObj')
+
+if numOfChannel == 3
+    save(new_filePath,'Ch1DataObj','Ch2DataObj','Ch3DataObj')
+else
+    save(new_filePath,'Ch1DataObj','Ch2DataObj','Ch3DataObj','Ch4DataObj')
+end
